8cc3b93f5514b8328a489fddd9a0d30d69e486e1
nafisfaysal <mirzanafisfaysal@gmail.com>
2021-05-01 12:36:02 +0600

    improved facebook ad headlines model

--- src/services/contents/facebook.contents.js
+++ src/services/contents/facebook.contents.js
@@ -26,19 +26,35 @@ List of 5 Primary texts:
 };
 
 const facebookAdHeadlines = async (userId, { platformType }) => {
+  // const prompt = `Catchy short Headlines for Facebook ad Samples:
+  // - Taste the grains. Build your body
+  // - Reignite your spark
+  // - Mapping Corona virus outbreak across the world
+  // - Feel & Finance Better
+  // - We ❤️ Pets. We 💣 Viruses
+  // - 30% Off First Subscription Order with Code X
+  // - Dentist Quality Night Guards
+  //
+
   const prompt = `Catchy short Headlines for Facebook ad Samples:
-- Taste the grains. Build your body
-- Reignite your spark
-- Mapping Corona virus outbreak across the world
-- Feel & Finance Better
-- We ❤️ Pets. We 💣 Viruses
-- 30% Off First Subscription Order with Code X
-- Dentist Quality Night Guards
+  - Shop the latest engagement right trends. [website_link].
+  - A CRAZY comfortable sneaker concealed pitching an elegant, luxurious shoe!.
+  - Discover the new X Collection.
+  - ENTIRE SITE is an Additional 60% OFF during our Labour Day Blowout Sale! Enter Promo Code.
+  - Adorable baby one-piece Soft & Cute Limited time up to 80% OFF.
+  - You have a limited offer of up to 80%.
+  - Hair Extensions, the way way to add instant length and volume.
+  - Be the perfect host with Royal Albert Dinnerware!
+  - Update Your closet for WINTER! Get AESTHETIC clothing at an affordable price.
+  - X socks are so comfortable, you won’t want to wear any other socks.
+  - One Bag. Every Lifestyle.
+  - I can’t express how incredible this watch looks on the wrist.
+  - Staying Healthy has never been more important!
+  - You bring the idea. We will help take it from there.
 
 Now write 5 catchy short Headline for following platform
 
 Platform: ${removeSpaces(platformType)}
-
 -`;
 
   const headlines = await generateContentUsingGPT3('davinci-instruct-beta', 500, prompt, 0.8, 0.2, 0.1, ['\n\n']);
@@ -66,7 +82,9 @@ List of 5 Ads:
 
 -`;
 
-  const adsFromProductDescription = await generateContentUsingGPT3('davinci-instruct-beta', 500, prompt, 0.8, 0.2, 0.4, ['\n\n']);
+  const adsFromProductDescription = await generateContentUsingGPT3('davinci-instruct-beta', 500, prompt, 0.8, 0.2, 0.4, [
+    '\n\n',
+  ]);
   return processListContents(userId, 'facebook-ads-from-product-description', prompt, adsFromProductDescription);
 };
 
