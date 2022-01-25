45cf6533d5402467cd4454c27d60d1ee7d8d63d6
Sabuj Ali <prosabuj98@gmail.com>
2022-01-25 12:26:45 +0600

    light packege added

--- src/config/plan.js
+++ src/config/plan.js
@@ -8,6 +8,8 @@ const trialInfo = {
 
 const subscription = {
   FREEMIUM: 'Freemium',
+  LIGHT_1MONTH: 'LIGHT_1MONTH',
+  LIGHT_6MONTH: 'LIGHT_6MONTH',
   BASIC_1MONTH: 'BASIC_1MONTH',
   BASIC_6MONTH: 'BASIC_6MONTH',
   PROFESSINAL_1MONTH: 'PROFESSINAL_1MONTH',
@@ -15,6 +17,8 @@ const subscription = {
 };
 
 const subscriptionPlan = {
+  [subscription.LIGHT_1MONTH]: { words: 13000, package: subscription.LIGHT_1MONTH },
+  [subscription.LIGHT_6MONTH]: { words: 13000 * 6, package: subscription.LIGHT_6MONTH },
   [subscription.BASIC_1MONTH]: { words: 70000, package: subscription.BASIC_1MONTH },
   [subscription.BASIC_6MONTH]: { words: 70000 * 6, package: subscription.BASIC_6MONTH },
   [subscription.PROFESSINAL_1MONTH]: { words: 300000, package: subscription.PROFESSINAL_1MONTH },
