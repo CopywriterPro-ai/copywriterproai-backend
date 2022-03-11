4cd7f1fe01f87ffd365913f7cb04601f0ac3d27f
Sabuj Ali <prosabuj98@gmail.com>
2022-03-11 22:42:07 +0600

    pricing plan data update

--- src/config/plan.js
+++ src/config/plan.js
@@ -21,8 +21,8 @@ const subscriptionPlan = {
   [subscription.BASIC_6MONTH]: { words: 70000 * 6, package: subscription.BASIC_6MONTH },
   [subscription.STANDARD_1MONTH]: { words: 50000, package: subscription.STANDARD_1MONTH },
   [subscription.STANDARD_6MONTH]: { words: 50000 * 6, package: subscription.STANDARD_6MONTH },
-  [subscription.PROFESSIONAL_1MONTH]: { words: 300000, package: subscription.PROFESSIONAL_1MONTH },
-  [subscription.PROFESSIONAL_6MONTH]: { words: 300000 * 6, package: subscription.PROFESSIONAL_6MONTH },
+  [subscription.PROFESSIONAL_1MONTH]: { words: 200000, package: subscription.PROFESSIONAL_1MONTH },
+  [subscription.PROFESSIONAL_6MONTH]: { words: 200000 * 6, package: subscription.PROFESSIONAL_6MONTH },
 };
 
 module.exports = { subscription, trial: trialInfo, subscriptionPlan };
