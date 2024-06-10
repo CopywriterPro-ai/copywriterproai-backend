e5c0868284e3c91e09467827cb782629c24a7f22
Nafis Faysal <mirzanafisfaysal@gmail.com>
2024-06-10 16:31:18 +0000

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,153 +1,66 @@
 const httpStatus = require('http-status');
-const { v1: uuidv1 } = require('uuid');
 const catchAsync = require('../utils/catchAsync');
-const { authService, userService, tokenService, interestService, emailService, subscriberService } = require('../services');
+const { authService, userService, tokenService, interestService, emailService } = require('../services');
 const { frontendUrl } = require('../config/config');
-const { authTypes } = require('../config/auths');
-const { subscription } = require('../config/plan');
-const User = require('../models/user.model');
 
-/**
- * Register a new user or login existing user.
- */
 const register = catchAsync(async (req, res) => {
-  const { email } = req.body;
-  let user = await userService.getUser({ email });
-
-  // If user already exists, directly login the user
-  if (user) {
-    const tokens = await tokenService.generateAuthTokens(user);
-    return res.status(httpStatus.OK).send({ status: httpStatus.OK, user, tokens });
-  }
-
-  // If user does not exist, create a new user
-  user = await userService.createUser(req.body);
-  await emailService.sendVerifyAccountEmailUsingToken({
-    id: user.id,
-    email: user.email,
-    name: { firstName: user.firstName, lastName: user.lastName },
-  });
-  res.status(httpStatus.CREATED).send({
-    status: httpStatus.CREATED,
-    message: 'Thank you for registration, please check your email for account verification link',
-  });
+  const user = await userService.createUser(req.body);
+  await emailService.sendVerifyAccountEmailUsingToken({ id: user.id, email: user.email });
+  res.status(httpStatus.CREATED).send({ user });
 });
 
-/**
- * Verify user account.
- */
 const verifyAccount = catchAsync(async (req, res) => {
   const { sub: userId, email } = req.token;
-  let user = await userService.getUser({ _id: userId, isVerified: false });
-
-  // If user is already verified, send appropriate response
+  const user = await userService.getUser({ _id: userId, isVerified: false });
   if (!user) {
-    return res.status(httpStatus.BAD_REQUEST).send({ status: httpStatus.BAD_REQUEST, message: 'User not found or already verified' });
+    return res.status(httpStatus.BAD_REQUEST).send({ message: 'User not found or already verified' });
   }
-
-  // Handle case where user with same email exists but not verified
-  const existingUser = await userService.getUser({ email, isVerified: true });
-  const userIdToUpdate = existingUser ? existingUser.userId : uuidv1();
-  const updatedUser = await userService.updateUserById(user.id, {
-    userId: userIdToUpdate,
-    isVerified: true,
-  });
-
-  await interestService.createUserInterest(email);
-
-  // Create subscription for new users
-  if (!existingUser) {
-    await subscriberService.createOwnSubscribe({ userId: updatedUser.userId, subscription: subscription.FREEMIUM });
-  }
-
-  // Delete unverified user
+  await interestService.createUserInterest(userId);
+  await userService.updateUserById(user, userId, { isVerified: true, bookmarks: {} });
   await userService.deleteunVerifiedUserByEmail(email);
-
-  await emailService.sendWelcomeEmail(email, user.firstName);
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, message: 'Your account is verified, please sign in' });
+  res.status(httpStatus.OK).send({ message: 'Your account is verified!' });
 });
 
-/**
- * Login user.
- */
 const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
-  const user = await authService.loginUser({ email, authType: authTypes.EMAIL }, password);
+  const user = await authService.loginUser({ email }, password);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, user, tokens });
+  res.send({ user, tokens });
 });
 
-/**
- * Logout user.
- */
 const logout = catchAsync(async (req, res) => {
   await authService.logout(req.body.refreshToken);
   res.status(httpStatus.NO_CONTENT).send();
 });
 
-/**
- * Refresh authentication tokens.
- */
 const refreshTokens = catchAsync(async (req, res) => {
   const tokens = await authService.refreshAuth(req.body.refreshToken);
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, ...tokens });
+  res.send({ ...tokens });
 });
 
-/**
- * Handle forgot password.
- */
 const forgotPassword = catchAsync(async (req, res) => {
   const { email } = req.body;
   await userService.registeredEmail(email);
   await emailService.sendResetPasswordEmailUsingToken(email);
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, message: 'An email has been sent to you with password reset instructions' });
+  res.status(httpStatus.OK).send({ message: 'An email has been sent to you with password reset instructions' });
 });
 
-/**
- * Reset user password.
- */
 const resetPassword = catchAsync(async (req, res) => {
   const { email } = req.token;
   await authService.resetPassword({ password: req.body.password, email });
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, message: 'Password reset successful, please sign in' });
+  res.status(httpStatus.OK).send({ message: 'Pasaword reset successful' });
 });
 
-/**
- * Handle OAuth strategy callback.
- */
 const strategyCallback = catchAsync(async (req, res) => {
   const { userId, authType, _id } = req.user;
   const jwtToken = tokenService.generateStrategyToken({ userId, authType, _id });
-  res.redirect(`${frontendUrl.web}/auth?token=${jwtToken}`);
+  res.redirect(`${frontendUrl.web}/?verifyClientToken=${jwtToken}`);
 });
 
-/**
- * Handle OAuth strategy login.
- */
 const strategyLogin = catchAsync(async (req, res) => {
   const user = await authService.strategyUser(req.user);
-  const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, user, tokens });
-});
-
-/**
- * Complete onboarding.
- */
-const completeOnboarding = catchAsync(async (req, res) => {
-  const user = await User.findByIdAndUpdate(req.user.id, { hasCompletedOnboarding: true }, { new: true });
-  res.status(httpStatus.OK).send({ status: httpStatus.OK, user });
-});
-
-/**
- * Submit user's own OpenAI API key.
- */
-const submitOwnOpenAIApiKey = catchAsync(async (req, res) => {
-  const { ownOpenAIApiKey } = req.body;
-  const user = await User.findByIdAndUpdate(req.user._id, { ownOpenAIApiKey }, { new: true });
-  if (!user) {
-    return res.status(httpStatus.NOT_FOUND).json({ error: 'User not found' });
-  }
-  return res.status(httpStatus.OK).json({ message: 'Own OpenAI API key submitted successfully', user });
+  const tokens = await tokenService.generateAuthTokens({ id: user.id });
+  res.status(httpStatus.OK).send({ user, tokens });
 });
 
 module.exports = {
@@ -160,6 +73,4 @@ module.exports = {
   resetPassword,
   strategyCallback,
   strategyLogin,
-  completeOnboarding,
-  submitOwnOpenAIApiKey,
 };
